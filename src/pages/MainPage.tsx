b394f21c63baf2bb050ccb4a087fcae530a407eb
Bas <480181@student.saxion.nl>
2024-05-17 10:22:21 +0200

    added way to call data and have it display on your webpage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Alert from "../components/Alert";
 import Button from "../components/Button";
+import axios from "axios";
 
 const MainPage = () => {
   // let list = ["magnum", "lick", "stegel", "loid", "charmander"];
@@ -14,12 +15,31 @@ const MainPage = () => {
 
   //we call the element alertVisible, which on default is false as seen in the useState(false) line
   //then when either onClose or onClick gets called the setter gets called
+  const [ip, setIp] = useState("");
+  const [ipVisible, setIpVisibility] = useState(false);
+
+  const fetchIp = async () => {
+    if (ipVisible) {
+      setIpVisibility(false);
+    } else {
+      try {
+        setIpVisibility(true);
+        const response = await axios.get("https://api.ipify.org/?format=json");
+        setIp(response.data.ip); //data.ip is because are are getting a json back
+      } catch (error) {
+        console.error("error");
+      }
+    }
+  };
+
   return (
     <div>
       {alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
       )}
       <Button onClick={() => setAlertVisibility(true)}>My button</Button>
+      <Button onClick={fetchIp}>IP button</Button>
+      {ipVisible && ip && <div>ip is: {ip}</div>}
     </div>
   );
 };
